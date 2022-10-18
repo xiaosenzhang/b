f32f07c07e700b651d9866d107a2a716f49de628
zhangxiaosen <zhangxiaosen@qijiayoudao.com>
2022-10-18 15:22:04 +0800

    feat: example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AButton></AButton>
+    <AInput></AInput>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AButton: () => import('remoteA/AButton'),
+    AInput: () => import('remoteA/AInput')
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ModuleFederationPlugin } = require("webpack").container
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    cache: {
+      type: 'filesystem'
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'B',
+        remotes: {
+          remoteA: 'App@http://localhost:8081/remoteEntry.js'
+        }
+      })
+    ]
+  }
 })
